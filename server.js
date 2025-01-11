9b8ed3c64a4bcd6fa0f2bce3d2e72ce1c8718644
Noa <noa.tournoy@supinfo.com>
2025-01-11 02:11:46 +0100

    relier commun.css vers pages html

--- server.js
+++ server.js
@@ -5,8 +5,10 @@ const app = express();
 
 const port = 3000;
 
+app.use(express.static(path.join(__dirname, 'src')));
+
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'src/pages/main.html'));
+    res.sendFile(path.join(__dirname, 'src/pages/index.html'));
 });
 
 app.listen(port, () => {
---
+++ src/scripts/head.js
@@ -0,0 +1,4 @@
+let commonStyles = document.createElement("link");
+commonStyles.rel = "stylesheet";
+commonStyles.href = "../styles/common.css";
+document.head.appendChild(commonStyles);
