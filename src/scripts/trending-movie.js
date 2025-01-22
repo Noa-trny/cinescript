5e0cb3948558ed3b44d94846d03ba43ac5033a5e
axel_lvn <axeloeven@icloud.com>
2025-01-22 14:37:10 +0100

    add: button more movies

--- src/scripts/trending-movie.js
+++ src/scripts/trending-movie.js
@@ -2,8 +2,18 @@ let pageNb = 0;
 const movieList = document.getElementById("movie-list");
 const apikey = "fa5a9170";
 const moviesToLoad = [
-  "tt13186482", "tt18259086", "tt26743414", "tt15239678", "tt11315808",
-  "tt28607951"
+  "tt13186482",
+  "tt18259086",
+  "tt26743414",
+  "tt15239678",
+  "tt11315808",
+  "tt28607951",
+  "tt1262426",
+  "tt29547746",
+  "tt15239678",
+  "tt9218128",
+  "tt27490099",
+  "tt30795948",
 ];
 
 function loadTrendingFilms() {
@@ -18,8 +28,8 @@ function loadTrendingFilms() {
 
     const url = `https://www.omdbapi.com/?apikey=${apikey}&i=${moviesToLoad[i]}`;
     fetch(url)
-      .then(response => response.json())
-      .then(data => {
+      .then((response) => response.json())
+      .then((data) => {
         const movieCard = document.createElement("a");
         movieCard.classList.add("movieCard");
         movieCard.href = `movie.html?id=${data.imdbID}`;
@@ -41,5 +51,19 @@ function loadTrendingFilms() {
   }
 }
 
-loadTrendingFilms();
+document.addEventListener("DOMContentLoaded", () => {
+  const movieBatch = 6;
+  loadTrendingFilms(); // Chargement initial
 
+  const loadMoreBtn = document.getElementById("load-more-btn");
+  if (loadMoreBtn) {
+    loadMoreBtn.addEventListener("click", () => {
+      if ((pageNb + 1) * movieBatch < moviesToLoad.length) {
+        pageNb++;
+        loadTrendingFilms();
+      } else {
+        loadMoreBtn.style.display = "none";
+      }
+    });
+  }
+});
